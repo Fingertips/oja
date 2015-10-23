4b04f07830deeaf50769e419535342718365675b
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2015-10-23 09:54:18 +0200

    Don't include .txt in `receipt_filename`

--- spec/oja_spec.rb
+++ spec/oja_spec.rb
@@ -3,38 +3,38 @@ require 'oja/mock'
 
 describe Oja do
   it "verifies receipt data" do
-    response = Oja.verify(receipt_data('receipt'))
+    response = Oja.verify(receipt_data('receipt.txt'))
     # The default for the mock response is success
     response.should.be.active
   end
 
   it "verifies receipt data with a password" do
-    response = Oja.verify(receipt_data('receipt'), 'secret')
+    response = Oja.verify(receipt_data('receipt.txt'), 'secret')
     # The default for the mock response is success
     response.should.be.active
   end
 
   it "verifies receipt data passed in an option hash" do
-    response = Oja.verify(:data => receipt_data('receipt'))
+    response = Oja.verify(:data => receipt_data('receipt.txt'))
     # The default for the mock response is success
     response.should.be.active
   end
 
   it "verifies an active receipt from disk" do
-    response = Oja.verify_filename(receipt_filename('receipt'))
+    response = Oja.verify_filename(receipt_filename('receipt.txt'))
     # The default for the mock response is success
     response.should.be.active
   end
 
   it "verifies an inactive receipt from disk" do
     Oja::Mock.responses << [200, { status: Oja::Response.status_code(:inactive) }]
-    response = Oja.verify_filename(receipt_filename('receipt'))
+    response = Oja.verify_filename(receipt_filename('receipt.txt'))
     response.should.be.inactive
   end
 
   it "does not return a response when the HTTP request fails" do
     Oja::Mock.responses << [500, {}]
-    response = Oja.verify_filename(receipt_filename('receipt'))
+    response = Oja.verify_filename(receipt_filename('receipt.txt'))
     response.should == nil
   end
 end
--- spec/receipt_spec.rb
+++ spec/receipt_spec.rb
@@ -2,28 +2,28 @@ require File.expand_path('../preamble', __FILE__)
 
 describe Oja::Receipt do
   it "loads a receipt from disk" do
-    receipt = Oja::Receipt.new(:filename => receipt_filename('receipt'))
-    receipt.data.should == receipt_data('receipt')
+    receipt = Oja::Receipt.new(:filename => receipt_filename('receipt.txt'))
+    receipt.data.should == receipt_data('receipt.txt')
   end
 
   it "accepts data for a receipt" do
-    receipt = Oja::Receipt.new(:data => receipt_data('receipt'))
-    receipt.data.should == receipt_data('receipt')
+    receipt = Oja::Receipt.new(:data => receipt_data('receipt.txt'))
+    receipt.data.should == receipt_data('receipt.txt')
   end
 
   it "includes the Base64 encoded receipt data in its attributes" do
-    data = receipt_data('receipt')
+    data = receipt_data('receipt.txt')
     receipt = Oja::Receipt.new(:data => data)
     receipt.attributes['receipt-data'].should == Base64.encode64(data)
   end
 
   it "includes the password in its attributes when it's configured" do
-    receipt = Oja::Receipt.new(:data => receipt_data('receipt'), :password => 'secret')
+    receipt = Oja::Receipt.new(:data => receipt_data('receipt.txt'), :password => 'secret')
     receipt.attributes['password'].should == 'secret'
   end
 
   it "includes the password in its JSON when it's configured" do
-    receipt = Oja::Receipt.new(:data => receipt_data('receipt'), :password => 'secret')
+    receipt = Oja::Receipt.new(:data => receipt_data('receipt.txt'), :password => 'secret')
     JSON.parse(receipt.to_json)['password'].should == 'secret'
   end
 end
